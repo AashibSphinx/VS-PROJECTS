4a87760cf40f46e2ea709c850dc75d3fc840fe8e
AashibSphinx <168278921+AashibSphinx@users.noreply.github.com>
2024-04-27 17:39:42 +0500

    Add files via upload

---
+++ VS PROJECTS/PROJECTS.TS.js
@@ -0,0 +1,16 @@
+// TASK 1
+var personName = "SAM";
+console.log("Hello ".concat(personName, ", would you like to learn some Python today?"));
+//TASK 2
+var personName2 = "SAM";
+console.log(personName2.toLowerCase());
+console.log(personName2.toUpperCase());
+console.log(personName2.charAt(0).toUpperCase() + personName2.slice(1).toLowerCase());
+//TASK 3  Famous Quote
+var quote = "A person who never made a mistake never tried anything new.";
+var author = "Albert Einstein";
+console.log("".concat(author, " once said, \"").concat(quote, "\""));
+//TASK 4  Famous Quote 2
+var famous_person = "Isaac Newton";
+var message = "".concat(famous_person, " once said, \"").concat(quote, "\"");
+console.log(message);
